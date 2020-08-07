01da516adf183ef1fd01b57d8d520ae3545cc739
Anyungu <anyungucw@gmail.com>
2020-08-07 16:14:08 +0300

    print on start again

--- auth-service-2/app/main/__init__.py
+++ auth-service-2/app/main/__init__.py
@@ -13,6 +13,8 @@ flask_bcrypt = Bcrypt()
 def create_app(config_name):
 
     print('...start')
+
+    print('...start again')
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
     db.init_app(app)
