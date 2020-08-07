4dc706d09e1c456cb430f1d6544c7ded412ddeb2
Anyungu <anyungucw@gmail.com>
2020-08-07 17:15:13 +0300

    fail test

--- auth-service-2/app/test/test_auth.py
+++ auth-service-2/app/test/test_auth.py
@@ -19,7 +19,7 @@ def register_user(self):
 
 def login_user(self):
     return self.client.post(
-        '/auth/login/2',
+        '/auth/login/',
         data=json.dumps(dict(
             email='example@gmail.com',
             password='123456'
