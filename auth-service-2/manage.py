e02a90a7cdb83e4256c38e7fe74048326a7cd007
Anyungu <anyungucw@gmail.com>
2020-08-18 08:17:27 +0300

    correction 308

--- auth-service-2/manage.py
+++ auth-service-2/manage.py
@@ -23,7 +23,7 @@ manager.add_command('db', MigrateCommand)
 
 @manager.command
 def run():
-    app.run(host="0.0.0.0", port=("8080"))
+    app.run(host="0.0.0.0", port="8080")
 
 
 @manager.command
